3e7cd47064e5f4aae476270cca238779ecbca2fe
Dileep Prithvi <34166409+Dileeprithvi@users.noreply.github.com>
2020-06-17 17:36:45 +0530

    Update httpd.sh

--- terraform-vpc-ec2/httpd.sh
+++ terraform-vpc-ec2/httpd.sh
@@ -2,4 +2,4 @@
 sudo yum install httpd -y
 sudo systemctl start httpd
 sudo systemctl enable httpd
-echo "<h1>Terraform Instance Launched Successfully</h1>" | sudo tee /var/www/html/index.html
+echo "<h1>Terraform Instance Launched Successfully from Public Subnet of Custom VPC!!!!!</h1>" | sudo tee /var/www/html/index.html
