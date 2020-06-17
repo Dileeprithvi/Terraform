813192fec6d411cf74328c52fb8e9e96173e5948
Dileep Prithvi <34166409+Dileeprithvi@users.noreply.github.com>
2020-06-17 17:31:35 +0530

    Update vpc.tf

--- terraform-vpc-ec2/vpc.tf
+++ terraform-vpc-ec2/vpc.tf
@@ -75,12 +75,18 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+# Defining the Elastic IP Address for NAT
+
+resource "aws_eip" "nat" {
+vpc      = true
+}
+
 # Defining the VPC NAT Gateway
 
 resource "aws_nat_gateway" "terraform_vpc_nat" {
   allocation_id = "${aws_eip.nat.id}"
   subnet_id     = "${aws_subnet.pub-sub1.id}"
-
+  depends_on = ["aws_internet_gateway.igw"]
   tags = {
     Name = "terraform_vpc_nat"
   }
