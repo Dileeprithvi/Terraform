04ff6ecd786b83e68f0aa05aa71db3222752f669
Dileep Prithvi <34166409+Dileeprithvi@users.noreply.github.com>
2020-06-18 14:44:16 +0530

    Delete httpd.sh

--- terraform-vpc-ec2/httpd.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-sudo yum install httpd -y
-sudo systemctl start httpd
-sudo systemctl enable httpd
-echo "<h1>Terraform Instance Launched Successfully from Public Subnet of Custom VPC!!!!!</h1>" | sudo tee /var/www/html/index.html
