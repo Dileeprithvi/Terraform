cb89677577486dfb84f8beeb7257887f0e963790
Dileep Prithvi <34166409+Dileeprithvi@users.noreply.github.com>
2020-06-18 14:42:43 +0530

    Delete httpd.sh

--- terraform-ec2-user-data/httpd.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-sudo yum install httpd -y
-sudo systemctl start httpd
-sudo systemctl enable httpd
-echo "<h1>Terraform Instance Launched Successfully</h1>" | sudo tee /var/www/html/index.html
